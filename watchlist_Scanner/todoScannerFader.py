67dac00075fcd20dc1bfe80f56fc34ca4ffd9ed0
james <james.anthony.russell36.com>
2024-12-29 14:41:51 +1100

    git

---
+++ watchlist_Scanner/todoScannerFader.py
@@ -0,0 +1,17 @@
+# todo
+
+# create a scanner that incorporates the fader and the channels into one scanning tool
+
+"""
+basically the price point is the main focus because I have limited resources as far as betting on the scan.
+so do a hirachy of importance
+
+1 the indicators, they have to have had a red green shift in the fader indicators that are currently printing a channel and the candle is on the lower channel line.
+2 the current hardest challenge is understanding the code to get this need solved the JMA is done as far as a python change.
+3 back to point. the current process is download the price ohlc vol and ticker name from yfinance
+4 once that data is downloaded into individual csv files we then want to update those files with a new data frame order.
+5 i am still trying to understand this change as i dont have a good background with csv files and conversions as to why you woould have the spaces empty with a api
+6 but i think imight have answered my own question cause the api will dowanload things without saving them which would account for the spaces being null
+
+
+"""
